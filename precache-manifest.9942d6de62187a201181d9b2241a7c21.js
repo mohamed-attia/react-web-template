474acdcc91466adea5a3b2064c20fe43fb870e9e
mohamed-attia <m.attia@outlook.sa>
2020-03-13 14:36:30 +0300

    Updates

---
+++ precache-manifest.9942d6de62187a201181d9b2241a7c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bc361d51eecb0ccbc73b9226a30f7c",
+    "url": "/react-web-template/index.html"
+  },
+  {
+    "revision": "327ae5ebe138fe234e55",
+    "url": "/react-web-template/static/css/main.b639c23b.chunk.css"
+  },
+  {
+    "revision": "e7fd0d57aa1eb0d88cec",
+    "url": "/react-web-template/static/js/2.9c597a7c.chunk.js"
+  },
+  {
+    "revision": "76bbb5dbcf32f9e8b66fee98c30112db",
+    "url": "/react-web-template/static/js/2.9c597a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327ae5ebe138fe234e55",
+    "url": "/react-web-template/static/js/main.87a70ad4.chunk.js"
+  },
+  {
+    "revision": "23134ee454e93844c2cc",
+    "url": "/react-web-template/static/js/runtime-main.2b91196a.js"
+  }
+]);
